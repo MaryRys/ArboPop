b3175563247b66436b339845f3a3ab38ac3530d2
unknown <rys.mary@gmail.com>
2019-07-28 17:26:25 -0500

    getting all samples

---
+++ ArboPop/Connections/SamplesConnections.cs
@@ -0,0 +1,92 @@
+﻿using ArboPop.Models.Sample;
+using Dapper;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArboPop.Connections
+{
+    public class SamplesConnections
+    {
+        readonly string ConnectionString;
+
+        public SamplesConnections(IOptions<DbConfiguration> dbConfig)
+        {
+            ConnectionString = dbConfig.Value.ConnectionString;
+        }
+
+        public IEnumerable<Sample> getAllSamples()
+        {
+            var connection = new SqlConnection(ConnectionString);
+            var queryString = @"SELECT *
+                                FROM [Sample]";
+            var samples = connection.Query<Sample>(queryString).ToList();
+            return samples;
+        }
+
+        //public complaint getSingleComplaint(int id)
+        //{
+        //    using (var connection = new SqlConnection(ConnectionString))
+        //    {
+        //        var queryString = @"SELECT *
+        //                            FROM [Activity]
+        //                            WHERE Id = @Id";
+        //        var complaint = connection.QueryFirstOrDefault<complaint>(queryString, new { id });
+        //        if (complaint != null)
+        //        {
+        //            return complaint;
+        //        }
+        //        throw new Exception("Can't find the submission you are looking for");
+        //    }
+        //}
+
+        //public IEnumerable<complaint> getAllUserComplaints(int userId)
+        //{
+        //    using (var connection = new SqlConnection(ConnectionString))
+        //    {
+        //        var queryString = @"SELECT *
+        //                        FROM [Activity]
+        //                        WHERE userId = @userId";
+        //        var products = connection.Query<complaint>(queryString, new { userId }).ToList();
+        //        return products;
+        //    }
+        //}
+
+        //public complaint AddNewComplaint(NewComplaint newComplaint)
+        //{
+        //    using (var connection = new SqlConnection(ConnectionString))
+        //    {
+        //        newComplaint.creationDate = DateTime.Now;
+        //        var queryString = @"INSERT into [Activity](userId, creationDate, timeOfDay, zipcode, bitingSource, addtlNotes)
+        //                            Output inserted.*
+        //                            Values(@userId, @creationDate, @timeOfDay, @zipcode, @bitingSource, @addtlNotes)";
+        //        var complaint = connection.QueryFirstOrDefault<complaint>(queryString, newComplaint);
+        //        if (complaint != null)
+        //        {
+        //            return complaint;
+        //        }
+        //        throw new Exception("Error, can't add the submission");
+        //    }
+        //}
+
+        //public complaint DeleteComplaint(int id)
+        //{
+        //    using (var connection = new SqlConnection(ConnectionString))
+        //    {
+        //        var queryString = @"DELETE 
+        //                            FROM [Activity]
+        //                            Output deleted.*
+        //                            WHERE id = @id";
+        //        var order = connection.QueryFirstOrDefault<complaint>(queryString, new { id });
+        //        if (order != null)
+        //        {
+        //            return order;
+        //        }
+        //        throw new Exception("Unable to delete that submission");
+        //    }
+        //}
+    }
+}
---
+++ ArboPop/Controllers/SamplesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArboPop.Connections;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArboPop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SamplesController : ControllerBase
+    {
+        readonly SamplesConnections _connections;
+
+        public SamplesController(SamplesConnections connections)
+        {
+            _connections = connections;
+        }
+
+
+        //use endpoint https://localhost:#####/api/Samples/all in url
+        [HttpGet("all")]
+        public ActionResult GetAllSamples()
+        {
+            var samples = _connections.getAllSamples();
+
+            return Ok(samples);
+        }
+
+        //// use endpoint https://localhost:#####/api/Complaints/{id} in url
+        //[HttpGet("{id}")]
+        //public ActionResult GetSingleComplaint(int id)
+        //{
+        //    var complaint = _connections.getSingleComplaint(id);
+
+        //    return Ok(complaint);
+        //}
+
+        //// use endpoint https://localhost:#####/api/Complaints/user/{userId} in url
+        //[HttpGet("user/{userId}")]
+        //public ActionResult GetAllUserOrders(int userId)
+        //{
+        //    var complaints = _connections.getAllUserComplaints(userId);
+
+        //    return Ok(complaints);
+        //}
+
+        //[HttpPost]
+        //public ActionResult AddComplaint(NewComplaint newComplaint)
+        //{
+        //    var createdComplaint = _connections.AddNewComplaint(newComplaint);
+
+        //    return Accepted(createdComplaint);
+
+        //}
+
+        //// pass id in body
+        //[HttpDelete("{id}")]
+        //public ActionResult DeleteComplaint(int id)
+        //{
+        //    var complaint = _connections.DeleteComplaint(id);
+        //    return Accepted(complaint);
+        //}
+    }
+}
