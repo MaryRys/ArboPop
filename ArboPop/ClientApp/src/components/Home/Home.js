c3d0430c2a61030b090a99c8b16df28368dc9294
unknown <rys.mary@gmail.com>
2019-07-30 20:08:24 -0500

    working on bootstrap

--- ArboPop/ClientApp/src/components/Home/Home.js
+++ ArboPop/ClientApp/src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
 import './Home.scss';
 
 class Home extends React.Component {
@@ -7,6 +8,7 @@ class Home extends React.Component {
         return(
         <div className="Home">
             <h1>You are Home</h1>
+            <Button color="primary">primary</Button>
             <img src="https://d17fnq9dkz9hgj.cloudfront.net/uploads/2013/09/cat-black-superstitious-fcs-cat-myths-162286659.jpg"></img>
 
         </div>
