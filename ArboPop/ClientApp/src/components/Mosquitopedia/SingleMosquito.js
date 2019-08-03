947032a586f3196d3716801e4d5317038b0b1864
unknown <rys.mary@gmail.com>
2019-08-03 13:53:40 -0500

    mosquito cards printing needs styling still

--- ArboPop/ClientApp/src/components/Mosquitopedia/SingleMosquito.js
+++ ArboPop/ClientApp/src/components/Mosquitopedia/SingleMosquito.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, Row, Col } from 'reactstrap';
 // import './SingleMosquito.scss';
 
 class SingleMosquito extends React.Component {
@@ -6,13 +8,21 @@ class SingleMosquito extends React.Component {
     const { mosquito } = this.props;
     return (
       <div className="MosquitoItem">
-        <div className="container">
-          <div className="row">
-                <h4>{mosquito.commonName}</h4>
-                <h4>{mosquito.scientificName}</h4>
-                <p>{mosquito.breedingHabitat}</p>
-                <p>{mosquito.confirmed}</p>
-          </div>
+          <div>
+            <Row>
+                <Card>
+                    <Col sm="6">
+                        <CardImg top width="100%" src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpeopledotcom.files.wordpress.com%2F2018%2F10%2Fbug.jpg&w=400&c=sc&poi=face&q=85" alt="Card image cap" />
+                        <CardBody>
+                        <CardTitle>{mosquito.scientificName}</CardTitle>
+                        <CardSubtitle>{mosquito.commonName}</CardSubtitle>
+                        <CardText>{mosquito.breedingHabitat}</CardText>
+                        <CardText>{mosquito.confirmed}</CardText>
+                        <Button>Learn More</Button>
+                        </CardBody>
+                    </Col>
+                </Card>
+            </Row>
         </div>
       </div>
     );
